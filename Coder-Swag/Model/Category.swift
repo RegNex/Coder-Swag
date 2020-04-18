eed9ecb7e8e0c39059d6acf734e74e70dc44393e
Etornam Sunu <sunumacbright@gmail.com>
2020-04-18 13:50:54 +0000

    Create Category.swift

---
+++ Coder-Swag/Model/Category.swift
@@ -0,0 +1,19 @@
+//
+//  Category.swift
+//  Coder-Swag
+//
+//  Created by Etornam Sunu on 4/18/20.
+//  Copyright © 2020 Etornam Sunu. All rights reserved.
+//
+
+import Foundation
+
+struct Category {
+   private(set) public var title: String
+   private(set) public var imageName: String
+    
+    init(title: String, imageName: String) {
+        self.title = title
+        self.imageName = imageName
+    }
+}
